f9a55487b97f3d7637c158d1f707b8c6aeedc024
Stepan Pazderka <stepan.pazderka@icloud.com>
2020-12-13 03:43:13 +0100

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Štěpán Pazderka on 13.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
