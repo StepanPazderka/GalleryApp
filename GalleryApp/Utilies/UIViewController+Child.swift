d2ae6e7b5eb8343ff593727099ee1e063f0d1f1c
Stepan Pazderka <stepan.pazderka@icloud.com>
2023-07-25 14:27:06 +0200

    FEATURE: Lightbox integration

---
+++ GalleryApp/Utilies/UIViewController+Child.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewController+Child.swift
+//  GalleryApp
+//
+//  Created by Štěpán Pazderka on 20.07.2023.
+//
+
+import Foundation
+import UIKit
+
+@nonobjc extension UIViewController {
+    func add(_ child: UIViewController, frame: CGRect? = nil) {
+        addChild(child)
+
+        if let frame = frame {
+            child.view.frame = frame
+        }
+
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+
+    func remove() {
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}
